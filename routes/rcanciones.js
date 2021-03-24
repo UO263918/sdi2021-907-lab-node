7c5438c0e1d76b4bccd293da249ffed2abedc257
Raúl López Calvo <raullopezcalvo25@gmail.com>
2021-03-24 22:46:30 +0100

    SDI-2021-907-7.5-Vistas y motores de plantillas.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,16 +1,33 @@
-module.exports = function(app) {
+module.exports = function(app, swig) {
     app.get("/canciones", function(req, res) {
-        let respuesta = "";
 
-        if (req.query.nombre != null)
-            respuesta += 'Nombre: ' + req.query.nombre + '<br>';
+        let canciones = [ {
+            "nombre" : "Blank space",
+            "precio" : "1.2"
+        }, {
+            "nombre" : "See you again",
+            "precio" : "1.3"
+        }, {
+            "nombre" : "Uptown Funk",
+            "precio" : "1.1"
+        }];
 
-        if (typeof (req.query.autor) != "undefined")
-            respuesta += 'Autor: '+ req.query.autor;
+        let respuesta = swig.renderFile('views/btienda.html', {
+            vendedor : 'Tienda de canciones',
+            canciones : canciones
+        });
 
         res.send(respuesta);
 
     });
+
+    app.get('/canciones/agregar', function (req, res) {
+        let respuesta = swig.renderFile('views/bagregar.html', {
+
+        });
+        res.send(respuesta);
+    });
+
 
     app.get('/suma', function(req, res) {
         let respuesta =  parseInt(req.query.num1) + parseInt(req.query.num2);
@@ -30,8 +47,8 @@ module.exports = function(app) {
 
     app.post("/cancion", function(req,res){
         res.send("Canción agregada: "+req.body.nombre +"<br>"
-                    +" genero : " +req.body.genero +"<br>"
-                +" precio: "+req.body.precio);
+            +" genero : " +req.body.genero +"<br>"
+            +" precio: "+req.body.precio);
     });
 
     app.get('/promo*', function (req, res) {
