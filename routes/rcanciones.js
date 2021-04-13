450dd95b1affd12b2d235a65e937bf31a0df2dbb
Raúl López Calvo <raullopezcalvo25@gmail.com>
2021-04-13 23:56:37 +0200

    SDI-2021-907-10.5-SW Consumir SW desde NodeJS.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -132,27 +132,43 @@ module.exports = function(app, swig, gestorBD) {
 
     app.get('/cancion/:id', function (req, res) {
         let autor = false;
-        let criterioCacnion  = {"_id": gestorBD.mongo.ObjectID(req.params.id)};
-        let criterioParaBusquedaDeComentarios= { "cancion_id" : gestorBD.mongo.ObjectID(req.params.id) };
+        let criterioCacnion = {"_id": gestorBD.mongo.ObjectID(req.params.id)};
+        let criterioParaBusquedaDeComentarios = {"cancion_id": gestorBD.mongo.ObjectID(req.params.id)};
         gestorBD.obtenerCanciones(criterioCacnion, function (canciones) {
             if (canciones == null) {
                 res.send("Error al obtener cancion");
             } else {
-                gestorBD.obtenerComentarios(criterioParaBusquedaDeComentarios, function(comentarios){
-                    if(comentarios == null){
+                gestorBD.obtenerComentarios(criterioParaBusquedaDeComentarios, function (comentarios) {
+                    if (comentarios == null) {
                         res.send("Error al obtener los comentarios");
                     }
 
-                    if (canciones[0].autor == req.session.usuario){
-                        author = true;
+                    if (canciones[0].autor == req.session.usuario) {
+                        autor = true;
                     }
-                    let respuesta = swig.renderFile('views/bcancion.html',{
-                        cancion: canciones[0],
-                        comentarios: comentarios,
-                        author : author
-                    });
-                    res.send(respuesta);
-                })
+                    let configuracion = {
+                        url: "https://www.freeforexapi.com/api/live?pairs=EURUSD",
+                        method: "get",
+                        headers: {
+                            "token": "ejemplo",
+                        }
+                    }
+                    let rest = app.get("rest");
+                    rest(configuracion, function (error, response, body) {
+                        console.log("cod: " + response.statusCode + " Cuerpo :" + body);
+                        let objetoRespuesta = JSON.parse(body);
+                        let cambioUSD = objetoRespuesta.rates.EURUSD.rate;
+                        // nuevo campo "usd"
+                        canciones[0].usd = cambioUSD * canciones[0].precio;
+                        let respuesta = swig.renderFile('views/bcancion.html',
+                            {
+                                cancion: canciones[0],
+                                comentarios: comentarios,
+                                author: autor
+                            });
+                        res.send(respuesta);
+                    })
+                });
             }
         });
     });
@@ -292,5 +308,5 @@ module.exports = function(app, swig, gestorBD) {
                 });
             }
         });
-    });
+    })
 };
